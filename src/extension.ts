f182b6c3abaa6470dd8977836ea3b7d0ac63b398
Lars Heinemann <lhein.smx@gmail.com>
2020-02-19 16:26:44 +0100

    added min/max name length check #59
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -93,7 +93,7 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('datavirt.create.vdb', (ctx) => {
 		if (workspaceReady) {
 			vscode.window.showInputBox( {validateInput: (name: string) => {
-				if (/^[a-z0-9-.]*$/.test(name)) {
+				if (/^[a-z0-9-.]{4,253}$/.test(name)) {
 					return undefined;
 				} else {
 					return "The entered name does not comply with the naming conventions. [a-z0-9-.]";
